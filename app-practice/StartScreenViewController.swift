d7f4f78fa842de0ea89a8c66c0ffa59b97a380a2
Felicia Hou <felicia.hou27@gmail.com>
2018-07-16 10:46:01 -0700

    added unit tests

--- app-practice/StartScreenViewController.swift
+++ app-practice/StartScreenViewController.swift
@@ -19,10 +19,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        outputDict()
         // Handle the text field's user input through delegate callbacks.
         greetingTextField.delegate = self
         showText()
+        outputDict()
+
     }
     
     func showText() {
@@ -36,12 +37,62 @@ class ViewController: UIViewController, UITextFieldDelegate {
             return " "
         }
     }
+    
+    func primeNumbers(num: Int) -> Bool {
+        var div = 0
+        for x in 1...num {
+            if num % x == 0 {
+                div += 1
+            }
+        }
+        if div > 2 {
+            return false
+        } else {
+            return true
+        }
+    }
+        
+    func primeList(num: Int) -> Array<Int> {
+        var primes: [Int] = []
+        for x in 2...num {
+            if primeNumbers(num: x) {
+                primes.append(x)
+            }
+        }
+        return primes
+    }
+    
+    func evenList(num: Int) -> Array<Int> {
+        var evens: [Int] = []
+        for x in 2...num {
+            if x % 2 == 0 {
+                evens.append(x)
+            }
+        }
+        return evens
+    }
+    
+    func oddList(num: Int) -> Array<Int> {
+        var odds: [Int] = []
+        for x in 1...num {
+            if x % 2 != 0 {
+                odds.append(x)
+            }
+        }
+        return odds
+    }
 
-    var numbers = ["prime": [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97],
-                   "even": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100],
-                   "odd": [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]]
+    func createDictionary() -> [String: Array<Int>] {
+        var numbers = [String: Array<Int>]()
+        numbers["prime"] = primeList(num: 100)
+        numbers["even"] = evenList(num: 100)
+        numbers["odd"] = oddList(num: 100)
+        return numbers
+    }
+    
+
     func outputDict() {
-        for (key, value) in numbers {
+        for (key, value) in createDictionary() {
             print("\(key) \t \(value)")
         }
     }
